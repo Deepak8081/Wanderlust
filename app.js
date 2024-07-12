60fb3a1f1027f306b050497ffa502aa6e94537a9
Deepak8081 <131394980+Deepak8081@users.noreply.github.com>
2024-07-12 12:53:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-
-if(process.env.NODE_ENV !="production"){
+if (process.env.NODE_ENV != "production") {
     require('dotenv').config();
 }
 
@@ -14,7 +13,7 @@ const listingsRouter = require("./routes/listing.js");
 const reviewsRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
 const session = require("express-session")
-const Mongo_Store=require("connect-mongo");
+const Mongo_Store = require("connect-mongo");
 const flash = require("connect-flash")
 const passport = require("passport")
 const LocalStrategy = require("passport-local");
@@ -22,10 +21,8 @@ const User = require("./models/user.js");
 
 
 
-app.listen(8000, () => {
-    console.log("server is listening port")
-})
-    const dbUrl=process.env.ATLASDB_URL;
+
+const dbUrl = process.env.ATLASDB_URL;
 // const MONGO_URL='mongodb://127.0.0.1:27017/wanderlust'
 async function main() {
     await mongoose.connect(dbUrl);
@@ -43,21 +40,21 @@ app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate)
 app.use(express.static(path.join(__dirname, "public")));
 
-const store=Mongo_Store.create({
-    mongoUrl:dbUrl,
-    crypto:{
-        secret:process.env.SECRET,
+const store = Mongo_Store.create({
+    mongoUrl: dbUrl,
+    crypto: {
+        secret: process.env.SECRET,
     },
-    touchAfter:24*3600,
+    touchAfter: 24 * 3600,
 })
 
-store.on("error",()=>{
-    console.log("error on mongo session store",err);
+store.on("error", () => {
+    console.log("error on mongo session store", err);
 })
 
 const sessionOptions = {
     store,
-    secret:process.env.SECRET,
+    secret: process.env.SECRET,
     resave: false,
     saveUninitialized: true,
     Cookie: {
@@ -86,7 +83,7 @@ passport.deserializeUser(User.deserializeUser());
 app.use((req, res, next) => {
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
-    res.locals.currUser=req.user;
+    res.locals.currUser = req.user;
     next();
 })
 
@@ -94,18 +91,18 @@ app.use("/listings", listingsRouter);
 app.use("/listings/:id/reviews", reviewsRouter)
 app.use("/", userRouter)
 
-app.all("*",(req,res,next)=>{
-    next(new ExpressError(404,"Page Not Found!"));
+app.all("*", (req, res, next) => {
+    next(new ExpressError(404, "Page Not Found!"));
 })
 
 
 
-app.use((err,req,res,next)=>{
-    let{statusCode=500,message="Something went wrong!"}=err;
-    res.status(statusCode).render("./listings/error.ejs",{message})
+app.use((err, req, res, next) => {
+    let { statusCode = 500, message = "Something went wrong!" } = err;
+    res.status(statusCode).render("./listings/error.ejs", { message })
     res.status(statusCode).send(message);
-    });
+});
 
-app.listen(8080, () => {
-     console.log("server is live")
+app.listen(8000, () => {
+    console.log("server is listening port")
 })
